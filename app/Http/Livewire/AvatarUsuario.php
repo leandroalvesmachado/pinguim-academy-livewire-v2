33e54a476d8b66a544b58ec62ccfce616cd51d06
leandroalvesmachado <leandroalvesmachado@gmail.com>
2023-11-13 22:42:39 -0300

    salvando

--- app/Http/Livewire/AvatarUsuario.php
+++ app/Http/Livewire/AvatarUsuario.php
@@ -2,12 +2,35 @@
 
 namespace App\Http\Livewire;
 
+use Illuminate\Support\Facades\Storage;
 use Livewire\Component;
+use Livewire\WithFileUploads;
 
 class AvatarUsuario extends Component
 {
+    use WithFileUploads;
+    public $avatar;
+
     public function render()
     {
         return view('livewire.avatar-usuario');
+    }
+
+    public function save()
+    {
+        $ref = $this->avatar->store('public');
+        $user = auth()->user();
+        $user->avatar = $ref;
+        $user->save();
+    }
+
+    public function download()
+    {
+        return Storage::download(auth()->user()->avatar);
+
+        // outra forma
+        // return response()->download(
+        //     storage_path('app/' . auth()->user()->avatar);
+        // );
     }
 }
--- resources/views/livewire/avatar-usuario.blade.php
+++ resources/views/livewire/avatar-usuario.blade.php
@@ -1,3 +1,27 @@
 <div>
-    {{-- In work, do what you enjoy. --}}
+  <x-secondary-button wire:click="download">
+    Download
+  </x-secondary-button>
+  <form wire:submit.prevent="save">
+    <div>
+      <x-input-label for="avatar" :value="__('Avatar')" />
+
+      @if ($avatar)
+        <img src="{{  $this->avatar->temporaryUrl() }}" />
+      @endif
+
+      <x-text-input
+        wire:model.defer="avatar"
+        id="avatar"
+        class="block mt-1 w-full"
+        type="file"
+        name="avatar"
+        :value="old('avatar')"
+      />
+      <x-input-error :messages="$errors->get('avatar')" class="mt-2" />
+    </div>
+    <x-primary-button>
+      Save
+    </x-primary-button>
+  </form>
 </div>
